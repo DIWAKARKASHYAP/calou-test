bf4dd50bd92b3895a8ad6c261fdfa723cc0cfa1f
DIWAKAR <didk9675@gmail.com>
2024-03-11 23:14:55 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ const HTMLContentTypes = ['html', 'htm']; // Sample array of HTML content types
 const IMGContentTypes = ['image/jpeg', 'image/png']; // Sample array of image content types
 
 export default function Home() {
-  const itemClasses = "sample-class"; // Sample class for the container div
+  const itemClasses = ""; // Sample class for the container div
   const item = {
       contentType: 'html', // Sample content type (you can change this for testing)
-      url: 'https://diwakar-portfolio.vercel.app/' // Sample URL (you can change this for testing)
+      url: 'https://www.example.com/' // Sample URL (you can change this for testing)
   };
   const _index = 1; // Sample index (you can change this for testing)
 
